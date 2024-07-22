eae87431b7ae18de62381e13164f9e1957381038
Francis Lagares <francis.lagares@gmail.com>
2024-07-22 22:02:52 +0200

    feat: create react-query provider

---
+++ client/src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,24 @@
+import { PropsWithChildren, useState } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ms from 'ms';
+
+const ReactQueryProvider = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: ms('12h'),
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
